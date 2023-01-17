686de2c94eaeedc276c94e3143f78a1a17058d0b
Marinus <marinus.fehling@web.de>
2023-01-17 09:08:16 +0100

    switch case fehler ausgebessert

--- challenge_02.js
+++ challenge_02.js
@@ -21,44 +21,46 @@ const Name = prompt("Wie heißt du denn?");
 const Alter = prompt("Wie alt bist du?");
 
 /*** Mit Switch-Case ****/
-// switch (true) {
-//     case (Alter >= 0) && (Alter < 6):
-//         console.log(Name + " " + "trinkt Milch");
-//         break;
-//     case (Alter >= 6) && (Alter < 13):
-//         console.log(Name + " " + "trinkt Saft");
-//         break;
-//     case (Alter >= 13) && (Alter < 18):
-//         console.log(Name + " " + "trinkt Cola");
-//         break;
-//     case (Alter >= 18):
-//         console.log(Name + " " + "trinkt Wein")
-//     default:
-//         console.log("Ungültiges Alter eingegeben");
-//         break;
-// }
+switch (true) {
+    case (Alter >= 0) && (Alter < 6):
+        console.log(Name + " " + "trinkt Milch");
+        break;
+    case (Alter >= 6) && (Alter < 13):
+        console.log(Name + " " + "trinkt Saft");
+        break;
+    case (Alter >= 13) && (Alter < 18):
+        console.log(Name + " " + "trinkt Cola");
+        break;
+    case (Alter >= 18):
+        console.log(Name + " " + "trinkt Wein");
+        break;
+    default:
+        console.log("Ungültiges Alter eingegeben");
+        break;
+}
+
 
 /*** Mit if-else ***/
-if(Alter >= 0 && Alter < 6)
-{
-    console.log(Name + " " + "trinkt Milch")
-}
-else if(Alter >= 6 && Alter < 13)
-{
-    console.log(Name + " " + "trinkt Saft");
-}
-else if(Alter >= 13 && Alter < 18)
-{
-    console.log(Name + " " + "trinkt Cola");
-}
-else if(Alter >= 18)
-{
-    console.log(Name + " " + "trinkt Wein");
-}
-else
-{
-    console.log("Ungültiges Alter eingegeben");
-}
+// if(Alter >= 0 && Alter < 6)
+// {
+//     console.log(Name + " " + "trinkt Milch")
+// }
+// else if(Alter >= 6 && Alter < 13)
+// {
+//     console.log(Name + " " + "trinkt Saft");
+// }
+// else if(Alter >= 13 && Alter < 18)
+// {
+//     console.log(Name + " " + "trinkt Cola");
+// }
+// else if(Alter >= 18)
+// {
+//     console.log(Name + " " + "trinkt Wein");
+// }
+// else
+// {
+//     console.log("Ungültiges Alter eingegeben");
+// }
 
 
 
